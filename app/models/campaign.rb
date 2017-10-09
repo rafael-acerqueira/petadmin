be94826000f94c926ca93095a6496bea7196947c
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-10-09 20:19:41 -0300

    Creating has_many association between sell and anothers models

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,7 +2,10 @@ class Campaign < ApplicationRecord
   include Fae::BaseModelConcern
 
   validates :title, presence: true
-  validates :body, presence: true  
+  validates :body, presence: true
+
+  has_many :campaign_clients
+  has_many :clients, through: :campaign_clients  
 
   def fae_display_field
     title
--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,9 +1,15 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
 
-  validates :client, presence: true  
+  validates :client, presence: true
 
   enum status: { finished: 0, canceled: 1 }
+
+  has_many :sell_products
+  has_many :products, through: :sell_products
+
+  has_many :sell_services
+  has_many :services, through: :sell_services  
 
   def fae_display_field
     id
