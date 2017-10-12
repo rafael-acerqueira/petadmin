1936381cf4cf749e6c3a28b2736b28c15b67fb0c
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-10-12 19:31:36 -0300

    Add address resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'  
   namespace :admin do
+    resources :addresses
     resources :products
     resources :services
     resources :sells
