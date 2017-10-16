61c247192f68c1c8afd5af68ca0d42ed88969635
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-10-16 18:54:14 -0300

    Add kind of discounts

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -3,10 +3,11 @@ class Discount < ApplicationRecord
 
   validates :title, uniqueness: true, presence: true
   validates :value, uniqueness: true, presence: true
+  validates :kind, presence: true
+
+  enum kind: { fixed: 0, percent: 1 }
 
   def fae_display_field
     title
   end
-
-
 end
