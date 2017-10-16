aa2e6b5c8ad96a7252bdc8005ebb9feff533b95b
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-10-16 17:44:18 -0300

    Send email when schedule a service with the schedule's informations

--- app/mailers/marketing_mailer.rb
+++ app/mailers/marketing_mailer.rb
@@ -3,4 +3,9 @@ class MarketingMailer < ApplicationMailer
     @body = body
     mail to: client.email, subject: title
   end
+
+  def schedule_service(schedule_service)
+    @schedule_service = schedule_service
+    mail to: schedule_service.client.email, subject: 'Schedule Services'
+  end
 end
